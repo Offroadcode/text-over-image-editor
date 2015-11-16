ce9aee4e4ba50e6be5f62f064d11e833f3b1be1f
Kyle Weems <kyle@mindfly.com>
2015-11-16 14:00:20 -0800

    Text Over Image package angular functionality.

--- TextOverImage/controllers/text.over.image.editor.controller.js
+++ TextOverImage/controllers/text.over.image.editor.controller.js
@@ -1,4 +1,4 @@
-angular.module("umbraco").controller("text.over.image.editor.controller", function($scope) {
+angular.module("umbraco").controller("text.over.image.editor.controller", function($scope, dialogService) {
 
 	// Initialization Methods ////////////////////////////////////////////////////
 
@@ -7,8 +7,179 @@ angular.module("umbraco").controller("text.over.image.editor.controller", functi
 	* @description Triggered on the controller loaded, kicks off any initialization functions.
 	*/
 	$scope.init = function() {
-
+        $scope.setVariables();
 	};
+
+    /**
+    * @method setVariables
+    * @description Sets up the initial variables for the view.
+    */
+    $scope.setVariables = function() {
+        $scope.model.value = $scope.getPropertyValue();
+        $scope.maxWidth = $scope.getMaxWidth();
+		$scope.propertyEditorMode = "edit";
+        console.info($scope.model.value);
+        console.info($scope.maxWidth);
+    };
+
+	// Event Handler Methods /////////////////////////////////////////////////////
+
+	/**
+	* @method changePos
+	* @param {string} position - tl, tc, tr, ml, mc, mr, bl, bm, br
+	* @description Changes the position of the text over the image.
+	*/
+	$scope.changePos = function(position) {
+		if (position) {
+			$scope.model.value.position = position;
+		}
+		$scope.toggleMode('edit');
+	};
+
+    /**
+    * @method $scope.handleMediaPickerSelection
+    * @param {Object} data - modal object returned by dialogService.mediaPicker().
+    * @description Event handler triggered by a media picker dialog. If there is an image selected, updates the $scope.model.value with the image's information.
+    */
+    $scope.handleMediaPickerSelection = function(data) {
+        if (data) {
+            console.info(data);
+            if (data.id) {
+                $scope.model.value.media.id = data.id;
+                $scope.model.value.media.url = data.image;
+                $scope.model.value.media.width = data.originalWidth;
+                $scope.model.value.media.height = data.originalHeight;
+            }
+        }
+    };
+
+    /**
+    * @method openMediaPicker
+    * @description Opens the media picker dialog, showing only images, and sends the data returned to $scope.handleMediaPickerSelection.
+    */
+    $scope.openMediaPicker = function() {
+        var options = {
+            onlyImages: true,
+            callback: $scope.handleMediaPickerSelection
+        };
+        dialogService.mediaPicker(options);
+    };
+
+	/**
+	* @method toggleMode
+	* @param {optional string} mode
+	* @description If a mode is provided, switch to that. Otherwise, toggle between edit and select.
+	*/
+	$scope.toggleMode = function(mode) {
+		if (mode) {
+			$scope.propertyEditorMode = mode;
+		} else {
+			if ($scope.propertyEditorMode === "edit") {
+				$scope.propertyEditorMode = "select";
+			} else {
+				$scope.propertyEditorMode = "edit";
+			}
+		}
+	};
+
+	// Helper Methods ////////////////////////////////////////////////////////////
+
+    /**
+    * @method getBackgroundStyle
+    * @returns {array of styles}
+    * @description Provides styles for the background image of the text over image editor view.
+    */
+    $scope.getBackgroundStyle = function() {
+        var styles = {
+            width: "800px",
+            height: "400px",
+            background: "black"
+        }
+        if ($scope.model.value.media) {
+            var media = $scope.model.value.media;
+            width = media.width;
+            height = media.height;
+            var ratio = height / width;
+            if (width > $scope.maxWidth) {
+                width = $scope.maxWidth;
+                height = ratio * width;
+            }
+            styles = {
+                width: width + "px",
+                height: height + "px",
+                background: "url(" + media.url + ")",
+                'background-size': "contain"
+            };
+        }
+        return styles;
+    };
+
+	/**
+	* @method getFrameStyle
+	* @returns {array of styles}
+	* @description Provides styles for the controller's div wrapper to set its width.
+	*/
+	$scope.getFrameStyle = function() {
+		var styles = {
+			width: "800px"
+		}
+		if ($scope.model.value.media) {
+			var media = $scope.model.value.media;
+            width = media.width;
+            if (width > $scope.maxWidth) {
+                width = $scope.maxWidth;
+            }
+            styles = {
+                width: width + "px",
+            };
+		}
+		return styles;
+	};
+
+    /**
+    * @method getMaxWidth
+    * @returns {integer}
+    * @description If $scope.model.config has a maxWidth value, return that. Otherwise, return 800.
+    */
+    $scope.getMaxWidth = function() {
+        var value = 800;
+        if ($scope.model && $scope.model.config) {
+            if ($scope.model.config.maxWidth != undefined) {
+                value = parseInt($scope.model.config.maxWidth, 10);
+            }
+        }
+        return value;
+    };
+
+    /**
+    * @method getPropertyValue
+    * @returns {textOverImage.Models.TextOverImage}
+    * @description If the $scope.model.value already exists, filter it through the model and return it. Elsewise, create a new, default model.
+    */
+    $scope.getPropertyValue = function() {
+        var value = new textOverImage.Models.TextOverImage();
+        if ($scope.model) {
+            if ($scope.model.value != undefined) {
+                value = new textOverImage.Models.TextOverImage($scope.model.value);
+            }
+        }
+        return value;
+    };
+
+    /**
+    * @method hasImageSelected
+    * @returns {bool}
+    * @description Returns true if there is a selected media image, otherwise returns false.
+    */
+    $scope.hasImageSelected = function() {
+        var hasImageSelected = false;
+        if ($scope.model && $scope.model.value) {
+            if ($scope.model.value.media.id != 0) {
+                hasImageSelected = true;
+            }
+        }
+        return hasImageSelected;
+    };
 
 	// Call $scope.init() ////////////////////////////////////////////////////////
 
--- TextOverImage/models/text.over.image.editor.models.js
+++ TextOverImage/models/text.over.image.editor.models.js
@@ -4,10 +4,64 @@
 	* @class TextOverImage
 	* @this TextOverImage
 	* @param {JSON} data
+	* @param {textOverImage.Models.Media} data.media
+	* @param {string} data.headline - A text headline that will overlap the image.
+	* @param {string} data.subheadline - A text subheadline that will overlap the image.
+	* @param {string} data.position - tl, tc, tr, ml, mc, mr, bl, bm, br.
 	* @description Class defining a Text Over Image Editor, which displays a selectable image, headline, sub-headline, and text position.
 	*/
 	models.TextOverImage = function(data) {
 		var self = this;
+		self.headline = "Headline";
+		self.media = new textOverImage.Models.Media();
+		self.subheadline = "Sub-Headline";
+		self.position = "tl";
+		if (data !== undefined) {
+			if (data.headline !== undefined) {
+				self.headline = data.headline;
+			}
+			if (data.media !== undefined) {
+				self.media = new textOverImage.Models.Media(data.media);
+			}
+			if (data.subheadline !== undefined) {
+				self.subheadline = data.subheadline;
+			}
+			if (data.position !== undefined) {
+				self.position = data.position;
+			}
+		}
+	};
+
+	/**
+	* @class Media
+	* @this Media
+	* @param {JSON} data
+	* @param {integer} data.id
+	* @param {string} data.url
+	* @param {integer} data.width
+	* @param {integer} data.height
+	* @description Class definining a media object for the text over image.
+	*/
+	models.Media = function(data) {
+		var self = this;
+		self.id = 0;
+		self.url = "";
+		self.width = 0;
+		self.height = 0;
+		if (data !== undefined) {
+			if (data.id !== undefined) {
+				self.id = data.id;
+			}
+			if (data.url !== undefined) {
+				self.url = data.url;
+			}
+			if (data.width !== undefined) {
+				self.width = data.width;
+			}
+			if (data.height !== undefined) {
+				self.height = data.height;
+			}
+		}
 	};
 
 }(window.textOverImage.Models = window.textOverImage.Models || {}));
